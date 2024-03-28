917d650d7b72e54ed83e55b636110673c34dd112
yhcho0405 <yhcho0405@kakao.com>
2024-03-28 17:10:35 +0900

    feat: #2 #3 #21

--- func.js
+++ func.js
@@ -6,16 +6,54 @@ function removeComment(code) {
         .replace(/\/\*[\s\S]*?\*\/|\/\/.*/g, '');
 }
 
+function hasAsyncLoop(code) {
+    const asyncLoopPatterns = [
+      /setInterval\s*\(/,
+      /setTimeout\s*\(/,
+      /requestAnimationFrame\s*\(/,
+      /process\.nextTick\s*\(/,
+      /setImmediate\s*\(/
+    ];
+  
+    for (const pattern of asyncLoopPatterns) {
+      if (pattern.test(code)) {
+        return true;
+      }
+    }
+  
+    return false;
+}
+
 var newInterval;
 var isFirst = true;
+var _mainLoop;
 
+/*
+
+// TODO : 
+_mainLoop = function() {
+    // TODO : 
+};
+// TODO : 
+
+
+*/
+
+// #21
 export function applyCode(userCode) {
     console.log(userCode);
     var code = removeComment(userCode); 
 
     if (!isFirst) clearInterval(newInterval);
     (function() {
-        eval(code);
+        if (!hasAsyncLoop(code)) {
+            eval(code);
+            newInterval = setInterval(() => {
+                _mainLoop();
+            }, 1);
+        } else {
+            console.log("사용자 정의 비동기 루프 사용 금지, _mainLoop만 사용.");
+        }
     })();
     isFirst = false;
 }
