429e52cd0b33107585de873e8effa4de7f5bd98a
Younghyo Cho <yhcho0405@kakao.com>
2024-04-06 16:43:47 +0900

    fix: document관련 내용 삭제

--- instructions.js
+++ instructions.js
@@ -1,4 +1,9 @@
 export const manageInstructions = (onCloseInstructions) => {
+  // let _engineDone = false;
+  // let _leftRotationDone = false;
+  // let _rightRotationDone = false;
+  // let _engineAndRotationDone = false;
+
   let _engineDone = false;
   let _leftRotationDone = false;
   let _rightRotationDone = false;
@@ -18,20 +23,20 @@ export const manageInstructions = (onCloseInstructions) => {
   const likelyTouchDevice = window.matchMedia("(any-pointer: coarse)").matches;
 
   const show = () => {
-    document.querySelector("#instructions").classList.add("show");
+  //   document.querySelector("#instructions").classList.add("show");
 
-    if (likelyTouchDevice) {
-      document.querySelector("#forKeyboard").remove();
-    } else {
-      document.querySelector("#forTouch").remove();
-    }
+  //   if (likelyTouchDevice) {
+  //     document.querySelector("#forKeyboard").remove();
+  //   } else {
+  //     document.querySelector("#forTouch").remove();
+  //   }
   };
 
   function close() {
     // May be called twice in checkDone() on devices with both touch support
     // and a keyboard
     if (!_hasClosedInstructionsVar) {
-      document.querySelector("#instructions").classList.remove("show");
+      // document.querySelector("#instructions").classList.remove("show");
       try {
         localStorage.setItem("closedInstructions", true);
       } catch {}
@@ -56,35 +61,35 @@ export const manageInstructions = (onCloseInstructions) => {
 
   const setEngineDone = () => {
     _engineDone = true;
-    document.querySelector("#engineCheck").classList.add("strikethrough");
-    document.querySelector("#engineCheck input").checked = true;
+    // document.querySelector("#engineCheck").classList.add("strikethrough");
+    // document.querySelector("#engineCheck input").checked = true;
     checkDone();
   };
 
   const setLeftRotationDone = () => {
     _leftRotationDone = true;
-    document
-      .querySelector("#rightRotationCheck")
-      .classList.add("strikethrough");
-    document.querySelector("#rightRotationCheck input").checked = true;
+    // document
+    //   .querySelector("#rightRotationCheck")
+    //   .classList.add("strikethrough");
+    // document.querySelector("#rightRotationCheck input").checked = true;
 
     checkDone();
   };
 
   const setRightRotationDone = () => {
     _rightRotationDone = true;
-    document.querySelector("#leftRotationCheck").classList.add("strikethrough");
-    document.querySelector("#leftRotationCheck input").checked = true;
+    // document.querySelector("#leftRotationCheck").classList.add("strikethrough");
+    // document.querySelector("#leftRotationCheck input").checked = true;
 
     checkDone();
   };
 
   const setEngineAndRotationDone = () => {
     _engineAndRotationDone = true;
-    document
-      .querySelector("#engineAndRotationCheck")
-      .classList.add("strikethrough");
-    document.querySelector("#engineAndRotationCheck input").checked = true;
+    // document
+    //   .querySelector("#engineAndRotationCheck")
+    //   .classList.add("strikethrough");
+    // document.querySelector("#engineAndRotationCheck input").checked = true;
     checkDone();
   };
 
