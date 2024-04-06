faeb64f30db538d2d88cf8c20336449c5c46e89b
Younghyo Cho <yhcho0405@kakao.com>
2024-04-06 16:39:28 +0900

    feat: interval loop 기능 삭제, animation frame과 integration

--- func.js
+++ func.js
@@ -3,7 +3,7 @@ import { logging, getVelocityX, getVelocityY, getAngle, getHeight, getRotationVe
 
 function removeComment(code) {
     return code
-        .replace(/\/\*[\s\S]*?\*\/|\/\/.*/g, '');
+    .replace(/\/\*[\s\S]*?\*\/|\/\/.*/g, '');
 }
 
 function hasAsyncLoop(code) {
@@ -14,32 +14,28 @@ function hasAsyncLoop(code) {
       /process\.nextTick\s*\(/,
       /setImmediate\s*\(/
     ];
-  
+
     for (const pattern of asyncLoopPatterns) {
       if (pattern.test(code)) {
         return true;
       }
     }
-  
+
     return false;
 }
 
 var newInterval;
 var isFirst = true;
-var _mainLoop;
+export var _mainLoop;
 
 /*
-
 // TODO : 
 _mainLoop = function() {
     // TODO : 
 };
 // TODO : 
-
-
 */
 
-// #21
 export function applyCode(userCode) {
     console.log(userCode);
     var code = removeComment(userCode); 
@@ -48,9 +44,9 @@ export function applyCode(userCode) {
     (function() {
         if (!hasAsyncLoop(code)) {
             eval(code);
-            newInterval = setInterval(() => {
-                _mainLoop();
-            }, 1);
+            // newInterval = setInterval(() => {
+            //     _mainLoop();
+            // }, 1);
         } else {
             console.log("사용자 정의 비동기 루프 사용 금지, _mainLoop만 사용.");
         }
