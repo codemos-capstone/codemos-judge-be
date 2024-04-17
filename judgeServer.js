5d649a95b399aa6c98fc98a329357b915eaa1dfe
Younghyo Cho <yhcho0405@kakao.com>
2024-04-18 04:18:17 +0900

    fix: api test링크 수정

--- judgeServer.js
+++ judgeServer.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-app.get("/", (req, res) => {
+app.get("/api/test", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
